198eedda4091dd0e3bad51ec2c44c0729e3f369c
ivanaa96 <ivana.stanic1@hotmail.com>
2022-06-03 13:11:29 +0200

    JS anonymous functions

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//variable scope
 let a = "variable outside the function";
 
 function scopeOfVariable() {
@@ -11,4 +12,18 @@ function scopeOfVariable() {
 	console.log(a); //visible
 }
 scopeOfVariable();
-console.log(q); //not visible, outside of the scope
+// console.log(q); //not visible, outside of the scope
+
+//anonymous function
+//1. Using the built-in map array function and an anonymous function, square all items in a number array
+let array = [1, 2, 3];
+console.log(array.map((el) => el * el));
+
+// 2. Write an IIFE function using an anonymous function which sums two numbers and logs the sum out to the console
+((el1, el2) => {
+	console.log(el1 + el2);
+})(5, 10);
+
+//3. Make this function an arrow function
+let timesTwo = (num) => num * 2;
+console.log(timesTwo(5));
