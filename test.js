3989d5334f6c1b7634303c5ce690784139309cde
ivanaa96 <ivana.stanic1@hotmail.com>
2022-06-03 08:40:19 +0200

    Tested JS variable scope

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-//test comment for init commit
-// test2
+let a = "variable outside the function";
+
+function scopeOfVariable() {
+	var q = "variable inside the function";
+	for (let i = 0; i <= 3; i++) {
+		var x = "test var variable";
+		let y = "test let variable";
+	}
+	console.log(x); //visible
+	// console.log(y); //outside of the scope
+	console.log(a); //visible
+}
+scopeOfVariable();
+console.log(q); //not visible, outside of the scope
